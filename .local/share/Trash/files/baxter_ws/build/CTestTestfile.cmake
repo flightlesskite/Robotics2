5fe0d7607841f40224fc53243ee1feeb993b71ea
Calvin <Calvin>
2018-06-13 23:27:50 +0000

    move baxter

---
+++ .local/share/Trash/files/baxter_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/rflab/baxter_ws/src
+# Build directory: /home/rflab/baxter_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
